81734308031911029b600c5de948aa9c2c91537f
Gabriele Toffanin <GabrieleToffanin@outlook.it>
2022-04-16 15:53:00 +0200

    Aggiungere i file di progetto.

---
+++ TDDFIzzBuzz/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDFIzzBuzz
+{
+    public class FizzBuzz
+    {
+        public string PrintFizzOrBuzz(int[] numbers)
+        {
+            string result = ParseNumbersToString(numbers);
+            return result;
+        }
+
+        private string ParseNumbersToString(int[] numbers)
+        {
+            string result = "";
+            foreach(var number in numbers)
+            {
+                if (number.Equals(3))
+                    result += "Fizz";
+                if (number.Equals(5))
+                    result += "Buzz";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TDDFIzzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDFIzzBuzz
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        private FizzBuzz _fizzBuzz;
+        [SetUp]
+        public void SetUp()
+        {
+            _fizzBuzz = new FizzBuzz();
+        }
+
+        [TestCase(new int[] {3}, "Fizz")]
+        [TestCase(new int[] {5}, "Buzz")]
+        [Test]
+        public void GetFizzIfThreeGetBuzzIfFive(int[] num, string expectedResult)
+        {
+            var result = _fizzBuzz.PrintFizzOrBuzz(num);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
