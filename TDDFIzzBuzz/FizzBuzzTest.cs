37b1f5f021953126247d2d8b46abe032d8032f3b
Gabriele Toffanin <GabrieleToffanin@outlook.it>
2022-04-16 16:22:02 +0200

    Added new test cases when the number just contains 3 or 5

--- TDDFIzzBuzz/FizzBuzzTest.cs
+++ TDDFIzzBuzz/FizzBuzzTest.cs
@@ -25,5 +25,23 @@ namespace TDDFIzzBuzz
             var result = _fizzBuzz.PrintFizzOrBuzz(num);
             Assert.That(result, Is.EqualTo(expectedResult));
         }
+
+        [TestCase(new int[] {1,2,3,4,5,6}, "1,2,Fizz,4,Buzz,Fizz")]
+        [TestCase(new int[] {7,8,9,10,11,12,13}, "7,8,Fizz,Buzz,11,Fizz,Fizz")]
+        [Test] 
+        public void GetFizzOrBuzzInIntArray(int[] numbers, string expectedResult)
+        {
+            var result = _fizzBuzz.PrintFizzOrBuzz(numbers);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase(new int[] {13}, "Fizz")]
+        [TestCase(new int[] {512}, "Buzz")]
+        [Test]
+        public void GetFizzOrBuzzIfContainsToo(int[] numbers, string expectedResult)
+        {
+            var result = _fizzBuzz.PrintFizzOrBuzz(numbers);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
     }
 }
